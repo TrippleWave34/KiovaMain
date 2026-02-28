d581e32cb0a7fe587a25189e2cabc55a00615c12
Tivan Bowesman-Quaye <67236259+TivanBowesman@users.noreply.github.com>
2026-02-28 14:58:03 +0000

    added endpoints for the auth

--- backend/main.py
+++ backend/main.py
@@ -6,3 +6,10 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+#Log In/ Register
+@app.post("/login")
+async def login():
+    return {"message": "Login endpoint"}
+@app.post("/register")
+async def register():
+    return {"message": "Register endpoint"}
